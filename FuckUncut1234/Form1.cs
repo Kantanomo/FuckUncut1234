91a68388952f7b190e57e209df031fb13efdffe4
Kantanomo <>
2020-08-29 13:38:32 -0500

    Initial commit

---
+++ FuckUncut1234/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FuckUncut1234
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(@"http://halo2pc.com");
+            File.WriteAllText(Application.StartupPath + "Nerd.txt", request.GetResponse().GetResponseStream().ToString());
+        }
+    }
+}
